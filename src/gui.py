d4c786a9d9f3455c7bb673da026d75174ae2c174
Pladdee <51334251+Pladdee@users.noreply.github.com>
2020-06-08 10:55:08 +0200

    Restructure File to add a new DetailedView

--- src/gui.py
+++ src/gui.py
@@ -3,152 +3,177 @@ import tkinter.font as tkFont
 import tkinter.ttk as ttk
 import time as t
 from PIL import ImageTk, Image
-from ttkthemes import ThemedStyle
 from src.weather import *
 from src.mqtt import greenhouse
 
 
-get_weather()
+# We have multiple views. To switch between them we have to use a controller -> class Application
+# for further information see: https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry("800x480+400+240")
+        self.title("Wetter")
 
-width = 800
-height = 480
+        self.widthV = 800
+        self.heightV = 480
 
-root = tk.Tk()
-root.geometry("800x480+400+240")
-root.title("Wetter")
-style = ThemedStyle(root)
-style.set_theme("breeze")
+        # Images for displaying the weather -> format 120x120
+        img_format = (120, 120)
+        self.img_sun = ImageTk.PhotoImage(Image.open('../images/sun.gif').resize(img_format))
+        self.img_rain = ImageTk.PhotoImage(Image.open('../images/rain.gif').resize(img_format))
+        self.img_clouds = ImageTk.PhotoImage(Image.open('../images/cloud.gif').resize(img_format))
+        self.img_cloudy = ImageTk.PhotoImage(Image.open('../images/cloudy.gif').resize(img_format))
+        self.img_few_clouds = ImageTk.PhotoImage(Image.open('../images/sunny.gif').resize(img_format))
+        self.img_thunderstorm = ImageTk.PhotoImage(Image.open('../images/thunderstorm.gif').resize(img_format))
+        self.img_snow = ImageTk.PhotoImage(Image.open('../images/snow.gif').resize(img_format))
+        self.img_fog = ImageTk.PhotoImage(Image.open('../images/fog.gif').resize(img_format))
 
-# Fonts
-font_family = 'Cambria'
-font_underline = tkFont.Font(family=font_family, size=22, underline=True)
+        # smaller Images for displaying glasshouse data & wind ->
+        small_img_format = (60, 60)
+        self.img_windy = ImageTk.PhotoImage(Image.open('../images/wind.gif').resize(small_img_format))
+        self.img_temperature = ImageTk.PhotoImage(Image.open('../images/temperature.gif').resize(small_img_format))
+        self.img_humidity = ImageTk.PhotoImage(Image.open('../images/humidity.gif').resize(small_img_format))
+        self.img_soil_moisture = ImageTk.PhotoImage(Image.open('../images/soil_moisture.gif').resize(small_img_format))
 
-# Images for displaying the weather -> format 120x120
-img_format = (120, 120)
-img_sun = ImageTk.PhotoImage(Image.open('../images/sun.gif').resize(img_format))
-img_rain = ImageTk.PhotoImage(Image.open('../images/rain.gif').resize(img_format))
-img_clouds = ImageTk.PhotoImage(Image.open('../images/cloud.gif').resize(img_format))
-img_cloudy = ImageTk.PhotoImage(Image.open('../images/cloudy.gif').resize(img_format))
-img_few_clouds = ImageTk.PhotoImage(Image.open('../images/sunny.gif').resize(img_format))
-img_thunderstorm = ImageTk.PhotoImage(Image.open('../images/thunderstorm.gif').resize(img_format))
-img_snow = ImageTk.PhotoImage(Image.open('../images/snow.gif').resize(img_format))
-img_fog = ImageTk.PhotoImage(Image.open('../images/fog.gif').resize(img_format))
+        # https://openweathermap.org/weather-conditions
+        # if main == Clouds, than look at the description (for a more precise weather forecast)
+        self.images = {
+            "Clear": self.img_sun,
+            "Rain": self.img_rain,
+            "Drizzle": self.img_rain,
+            "overcast clouds": self.img_clouds,
+            "broken clouds": self.img_clouds,
+            "scattered clouds": self.img_cloudy,
+            "few clouds": self.img_few_clouds,
+            "Thunderstorm": self.img_thunderstorm,
+            "Snow": self.img_snow,
+            "Mist": self.img_fog,
+            "Smoke": self.img_fog,
+            "Haze": self.img_fog,
+            "Dust": self.img_fog,
+            "Fog": self.img_fog,
+            "Sand": self.img_fog,
+            "Ash": self.img_fog,
+            "Squall": self.img_fog,
+            "Tornado": self.img_fog
+        }
 
-# smaller Images for displaying glasshouse data & wind ->
-small_img_format = (60, 60)
-img_windy = ImageTk.PhotoImage(Image.open('../images/wind.gif').resize(small_img_format))
-img_temperature = ImageTk.PhotoImage(Image.open('../images/temperature.gif').resize(small_img_format))
-img_humidity = ImageTk.PhotoImage(Image.open('../images/humidity.gif').resize(small_img_format))
-img_soil_moisture = ImageTk.PhotoImage(Image.open('../images/soil_moisture.gif').resize(small_img_format))
+        # define fonts
+        font_family = 'Cambria'
+        self.font_underline = tkFont.Font(family=font_family, size=22, underline=True)
 
-# https://openweathermap.org/weather-conditions
-# if main == Clouds, than look at the description (for a more precise weather forecast)
-images = {
-    "Clear": img_sun,
-    "Rain": img_rain,
-    "Drizzle": img_rain,
-    "overcast clouds": img_clouds,
-    "broken clouds": img_clouds,
-    "scattered clouds": img_cloudy,
-    "few clouds": img_few_clouds,
-    "Thunderstorm": img_thunderstorm,
-    "Snow": img_snow,
-    "Mist": img_fog,
-    "Smoke": img_fog,
-    "Haze": img_fog,
-    "Dust": img_fog,
-    "Fog": img_fog,
-    "Sand": img_fog,
-    "Ash": img_fog,
-    "Squall": img_fog,
-    "Tornado": img_fog
-}
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
+        # stack all the views on top of one another, the one on top will be shown
+        self.frames = {}
+        for F in (MainView, DetailedView):
+            frame = F(container, self)
+            self.frames[F.__name__] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
-class TodayContainerV2:
-    def __init__(self, parent, today):
-        self.day = today
-        self.frame = ttk.Frame(parent, width=width / 4, height=height / 2, relief='groove', borderwidth=1)
-        self.icon = ttk.Label(self.frame, image=images.get(today.description))
-        self.icon.bind('<Button-1>', get_details)
-        self.max = tk.Label(self.frame, text=(str(today.max_temp) + '/'), font=("Cambria", 20))
-        self.min = tk.Label(self.frame, text=str(today.min_temp), font=("Cambria", 18), fg='gray')
-        self.frame.pack_propagate(0)
-        if type(self) is TodayContainerV2:
-            self.frame.pack_propagate(0)
-            self.frame.config(width=width / 3, relief='flat')
-            self.icon.config(image=images.get(today.current_description))
-            self.info = ttk.Frame(self.frame)
-            self.today_label = ttk.Label(self.info, text='Aktuell:', font=font_underline)
-            self.current_temp = ttk.Label(self.info, text=(str(today.current_temperature) + '°C'), font=("Cambria", 20))
-            self.pack()
+        # show the main view and remember which one is currently shown so that we know which view we have to update
+        self.active_frame = self.frames[MainView.__name__]
+        self.show_frame(None, "MainView", None)
 
-    def pack(self):
-        self.frame.pack(side='right', pady=(30, 0))
-        self.info.pack(side='left', fill='y')
-        self.today_label.pack(side='top')
-        self.current_temp.pack(side='top')
-        self.icon.pack()
-        self.max.pack(side='left', padx=(54, 0))
-        self.min.pack(side='left', pady=(9, 0))
+    def show_frame(self, event, page_name, day):
+        """Show a frame for the given page name"""
+        frame = self.frames[page_name]
+        # tkraise puts the frame on top of all of them, so that it can be shown
+        frame.tkraise()
+        self.active_frame = frame
+        # if we want to see details of the day we have to update the widgets of the frame
+        if page_name != MainView.__name__:
+            self.active_frame.change_to_day(day)
 
-    def update(self):
-        if type(self) is TodayContainerV2:
-            self.current_temp.config(text=str(self.day.current_temperature) + '°C')
-        self.icon.config(image=images.get(self.day.description))
-        self.max.config(text=str(self.day.max_temp) + '/')
-        self.min.config(text=str(self.day.min_temp))
+    # loop; update the active frame every 5 seconds
+    def update_view(self):
+        self.active_frame.update()
+        self.after(5000, self.update_view)
 
 
-class DayContainerV2(TodayContainerV2):
-    def __init__(self, parent, day):
-        TodayContainerV2.__init__(self, parent, day)
-        self.weekday = ttk.Label(self.frame, text=day.weekday, font=("Cambria", 22))
-        self.pack()
+class DayContainerV2(tk.Frame):
+    """Custom tk.Frame for proper representation of the day information"""
+    def __init__(self, parent, day, controller):
+        tk.Frame.__init__(self, parent, width=controller.widthV / 4, height=controller.heightV / 2, relief='groove',
+                          borderwidth=1)
+        self.day = day
+        self.controller = controller
+        self.icon = ttk.Label(self, image=controller.images.get(day.description))
+        # bind the call of another view to each icon. The new view shows more detailed information
+        self.icon.bind('<Button-1>', lambda event, page_name="DetailedView", d=day: controller.show_frame(event,
+                                                                                                          page_name, d))
+        self.max = tk.Label(self, text=(str(day.max_temp) + '/'), font=("Cambria", 20))
+        self.min = tk.Label(self, text=str(day.min_temp), font=("Cambria", 18), fg='gray')
+        self.weekday = ttk.Label(self, text=day.weekday, font=("Cambria", 22))
 
-    def pack(self):
-        self.frame.pack(side='left', pady=(30, 0))
+        # keep everything in place -> extremely static gui
+        self.pack_propagate(0)
+        # show everything
+        self.pack(side='left', pady=(30, 0))
         self.weekday.pack(side='bottom', pady=(0, 10))
         self.icon.pack(pady=(5, 0))
         self.max.pack(side='left', padx=(62, 0))
         self.min.pack(side='left', pady=(9, 0))
 
     def update(self):
-        TodayContainerV2.update(self)
-        self.weekday.config(text=self.day.weekday)
+        """update the widgets with new information"""
+        self.icon.config(image=self.controller.images.get(self.day.description))
+        self.max.config(text=str(self.day.max_temp) + '/')
+        self.min.config(text=str(self.day.min_temp))
 
 
-class ImageAndValue:
+class TodayContainerV2(DayContainerV2):
+    """Specialized DayContainerV2; adds information of the current situation to the view"""
+    def __init__(self, parent, today, controller):
+        DayContainerV2.__init__(self, parent, today, controller)
+        self.config(width=controller.widthV / 3, relief='flat')
+        self.icon.config(image=controller.images.get(today.current_description))
+
+        self.info = ttk.Frame(self)
+        self.today_label = ttk.Label(self.info, text='Aktuell:', font=controller.font_underline)
+        self.current_temp = ttk.Label(self.info, text=(str(today.current_temperature) + '°C'), font=("Cambria", 20))
+
+        # This Frame differs in size from the normal one. We have to reorder and repack everything
+        self.icon.forget()
+        self.max.forget()
+        self.min.forget()
+        self.weekday.forget()
+
+        self.pack(side='right', pady=(30, 0))
+        self.info.pack(side='left', fill='y')
+        self.today_label.pack(side='top')
+        self.current_temp.pack(side='top')
+        self.icon.pack(pady=(5, 0), fill='x')
+        self.max.pack(side='left', padx=(30, 0))
+        self.min.pack(side='left', pady=(9, 0))
+
+    def update(self):
+        """Update the inherited and new widgets"""
+        DayContainerV2.update(self)
+        self.current_temp.config(text=str(self.day.current_temperature) + '°C')
+
+
+class ImageAndValue(tk.Frame):
+    """Frame for representation of a special data. Used for Temperature, Humidity and Soil Moisture"""
     def __init__(self, parent, image, value, unit='%'):
-        self.frame = self.frame = ttk.Frame(parent)
-        self.icon = ttk.Label(self.frame, image=image)
+        tk.Frame.__init__(self, parent)
+        self.icon = ttk.Label(self, image=image)
+        # There is no keyboard at my Raspberry Pi. I use a click on the small icons to quit the application
+        # I admit that its not the best way to quit
         self.icon.bind('<Button-1>', destroy_gui)
         self.unit = unit
-        self.value = ttk.Label(self.frame, text=': ' + str(value) + str(unit), font=("Cambria", 22))
-        self.frame.pack(side='top')
+        self.value = ttk.Label(self, text=': ' + str(value) + str(unit), font=("Cambria", 22))
+        self.pack(side='top')
         self.icon.pack(side='left', pady=(5, 5))
         self.value.pack(side='left', pady=(5, 5))
 
     def update(self, value):
-        self.value.config(text=str(value) + self.unit)
-
-
-def update():
-    today_frame.update()
-    day1_frame.update()
-    day2_frame.update()
-    day3_frame.update()
-    day4_frame.update()
-    glasshouse_temperature.update(greenhouse.temperature)
-    glasshouse_humidity.update(greenhouse.humidity)
-    glasshouse_soil_moisture.update(greenhouse.soil_moisture)
-    current_date.config(text=day0.date)
-    current_time.config(text=t.strftime('%H:%M', t.localtime()) + ' Uhr')
-    root.after(5000, update)
-
-
-def get_details(event):
-    pass
+        """Update the value-widget"""
+        self.value.config(text=':' + str(value) + self.unit)
 
 
 def destroy_gui(event):
@@ -156,50 +181,92 @@ def destroy_gui(event):
     root.destroy()
 
 
-main_frame = ttk.Frame(root)
-upper_half = ttk.Frame(main_frame, width=width, height=height / 2)
-lower_half = ttk.Frame(main_frame, width=width, height=height / 2)
+class MainView(tk.Frame):
+    """The View which is shown most of the time"""
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        upper_half = ttk.Frame(self, width=controller.widthV, height=controller.heightV / 2)
+        lower_half = ttk.Frame(self, width=controller.widthV, height=controller.heightV / 2)
 
-main_frame.pack()
-upper_half.pack_propagate(0)
-lower_half.pack_propagate(0)
-upper_half.pack(side='top')
-lower_half.pack(side='bottom')
+        self.pack()
+        upper_half.pack_propagate(0)
+        lower_half.pack_propagate(0)
+        upper_half.pack(side='top')
+        lower_half.pack(side='bottom')
 
-# ----------------------    UPPER HALF  --------------------------------------------------------------------------------
+        # ---------------------------------------   UPPER HALF   -------------------------------------------------------
 
-glasshouse_frame = ttk.Frame(upper_half, width=width / 4 + 20, height=height / 2)
-glasshouse_frame.pack_propagate(0)
-glasshouse_frame.pack(side='left')
-glasshouse_Label = ttk.Label(glasshouse_frame, text='Im Gewächshaus', font=font_underline)
-glasshouse_Label.pack(side='top', pady=(5, 5))
-glasshouse_temperature = ImageAndValue(glasshouse_frame, img_temperature, greenhouse.temperature, unit='°C')
-glasshouse_humidity = ImageAndValue(glasshouse_frame, img_humidity, greenhouse.humidity)
-glasshouse_soil_moisture = ImageAndValue(glasshouse_frame, img_soil_moisture, greenhouse.soil_moisture)
+        glasshouse_frame = ttk.Frame(upper_half, width=controller.widthV / 4 + 20, height=controller.heightV / 2)
+        glasshouse_frame.pack_propagate(0)
+        glasshouse_frame.pack(side='left')
+        glasshouse_label = ttk.Label(glasshouse_frame, text='Im Gewächshaus', font=controller.font_underline)
+        glasshouse_label.pack(side='top', pady=(5, 5))
+        self.glasshouse_temperature = ImageAndValue(glasshouse_frame, controller.img_temperature,
+                                                    greenhouse.temperature, unit='°C')
+        self.glasshouse_humidity = ImageAndValue(glasshouse_frame, controller.img_humidity, greenhouse.humidity)
+        self.glasshouse_soil_moisture = ImageAndValue(glasshouse_frame, controller.img_soil_moisture,
+                                                      greenhouse.soil_moisture)
 
-today_frame = TodayContainerV2(upper_half, day0)
+        self.today_frame = TodayContainerV2(upper_half, day0, controller)
 
-date_time_frame = ttk.Frame(upper_half)
-current_time = ttk.Label(date_time_frame, text=str(t.strftime('%H:%M', t.localtime()) + ' Uhr'), font=("Cambria", 46))
-current_date = ttk.Label(date_time_frame, text=day0.date, font=("Cambria", 24))
-current_weekday = ttk.Label(date_time_frame, text=day0.weekday, font=("Cambria", 38))
-date_time_frame.pack()
-current_weekday.pack(side='top', pady=(30, 0))
-current_date.pack(side='top')
-current_time.pack(side='top', pady=(20, 0))
+        date_time_frame = ttk.Frame(upper_half)
+        self.current_time = ttk.Label(date_time_frame, text=str(t.strftime('%H:%M', t.localtime()) + ' Uhr'),
+                                      font=("Cambria", 46))
+        self.current_date = ttk.Label(date_time_frame, text=day0.date, font=("Cambria", 24))
+        self.current_weekday = ttk.Label(date_time_frame, text=day0.weekday, font=("Cambria", 38))
+        date_time_frame.pack()
+        self.current_weekday.pack(side='top', pady=(30, 0))
+        self.current_date.pack(side='top')
+        self.current_time.pack(side='top', pady=(20, 0))
 
-# ----------------------    LOWER HALF  --------------------------------------------------------------------------------
+        # -------------------------------------    LOWER HALF   --------------------------------------------------------
 
-day1_frame = DayContainerV2(lower_half, day1)
-day2_frame = DayContainerV2(lower_half, day2)
-day3_frame = DayContainerV2(lower_half, day3)
-day4_frame = DayContainerV2(lower_half, day4)
+        self.day1_frame = DayContainerV2(lower_half, day1, controller)
+        self.day2_frame = DayContainerV2(lower_half, day2, controller)
+        self.day3_frame = DayContainerV2(lower_half, day3, controller)
+        self.day4_frame = DayContainerV2(lower_half, day4, controller)
+
+    def update(self):
+        print('updating MainView...')
+        self.today_frame.update()
+        self.day1_frame.update()
+        self.day2_frame.update()
+        self.day3_frame.update()
+        self.day4_frame.update()
+        self.glasshouse_temperature.update(greenhouse.temperature)
+        self.glasshouse_humidity.update(greenhouse.humidity)
+        self.glasshouse_soil_moisture.update(greenhouse.soil_moisture)
+        self.current_date.config(text=day0.date)
+        self.current_time.config(text=t.strftime('%H:%M', t.localtime()) + ' Uhr')
+        self.current_weekday.config(text=day0.weekday)
+
+
+class DetailedView(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.day = None
+        self.weekday = tk.Label(self, text="You should never see this text", font=controller.font_underline)
+        self.weekday.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Zurück",
+                            command=lambda: controller.show_frame(None, "MainView", None))
+        button1.pack()
+
+    def change_to_day(self, day):
+        print(day)
+        self.day = day
+        self.weekday.config(text=day.weekday)
+
+    def update(self):
+        print("updating DetailedView...")
+
+
+get_weather()
+root = Application()
 
 # root.attributes('-fullscreen', True)
 if __name__ == '__main__':
     while True:
-        try:
-            update()
-            root.update()
-        except:
-            break
+        root.update()
